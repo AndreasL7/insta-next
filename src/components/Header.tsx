8d5baa1f9f3c3c83723e2a7e3b4bfc1c91a0ecc3
Andreas Lukita <andreas030503@gmail.com>
2024-12-06 13:27:09 +0800

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        <Link href={"/"} className="hidden lg:inline-flex">
+          <Image
+            src="/Instagram_logo_black.webp"
+            alt="logo"
+            width={96}
+            height={96}
+          />
+        </Link>
+        <Link href={"/"} className="lg:hidden">
+          <Image
+            src="/800px-Instagram_logo_2016.webp"
+            alt="logo"
+            width={96}
+            height={96}
+          />
+        </Link>
+
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
+        />
+
+        <button className="text-sm font-semibold text-blue-500">Log In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
